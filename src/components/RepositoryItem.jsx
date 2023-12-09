0ef70a214bd0d8cb44bce62d56c027795d96887d
Melodi Balossino <melodibalossino@gmail.com>
2023-12-09 20:01:57 -0300

    RepositoryItemHeader-component

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,21 +3,7 @@ import { StyleSheet, View, Image } from 'react-native'
 import StyledText from './StyledText'
 import RepositoryStats from './RepositoryStats'
 import theme from '../theme'
-
-const RepositoryItemHeader = ({ ownerAvatarUrl, fullName, description, language }) => {
-    return (
-        <View style={{ flexDirection: 'row', paddingBottom: 2 }}>
-            <View style={{ paddingRight: 10 }}>
-            <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
-            </View>
-            <View style={{ flex: 1}}>
-            <StyledText fontWeight='bold'>{fullName}</StyledText>
-            <StyledText color='textSecondary'>{description}</StyledText>
-            <StyledText style={styles.language}>{language}</StyledText>
-        </View>
-        </View>
-    )
-}
+import RepositoryItemHeader from './RepositoryItemHeader'
 
 const RepositoryItem = (props) => {
     return (
@@ -33,19 +19,6 @@ const styles = StyleSheet.create({
         padding: 20,
         paddingBottom: 5,
         paddingTop: 5,
-    },
-    language: {
-        padding: 5,
-        color: theme.colors.white,
-        backgroundColor: theme.colors.primary,
-        alignSelf: 'flex-start',
-        borderRadius: 4,
-        overflow: 'hidden',
-    },
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 4,
     },
 });
 
---
+++ src/components/RepositoryItemHeader.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import theme from '../theme'
+import StyledText from './StyledText'
+
+const RepositoryItemHeader = ({ ownerAvatarUrl, fullName, description, language }) => {
+    return (
+        <View style={{ flexDirection: 'row', paddingBottom: 2 }}>
+            <View style={{ paddingRight: 10 }}>
+            <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+            </View>
+            <View style={{ flex: 1 }}>
+            <StyledText fontWeight='bold'>{fullName}</StyledText>
+            <StyledText color='textSecondary'>{description}</StyledText>
+            <StyledText style={styles.language}>{language}</StyledText>
+        </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    language: {
+        padding: 5,
+        color: theme.colors.white,
+        backgroundColor: theme.colors.primary,
+        alignSelf: 'flex-start',
+        borderRadius: 4,
+        overflow: 'hidden',
+        marginTop: 5,
+        marginBottom: 5,
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 4,
+    },
+});
+
+export default RepositoryItemHeader
