2015906082ff9942753c1d061b815ff51501aaca
Melodi Balossino <melodibalossino@gmail.com>
2023-12-11 20:27:54 -0300

    Routing

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,28 +1,47 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import StyledText from "./StyledText";
 import Constants from "expo-constants";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: theme.appBar.primary,
         paddingTop: Constants.statusBarHeight + 10,
-        paddingBottom: 10,
-        paddingLeft: 10,
+        flexDirection: "row",
+    },
+    scroll: {
+        paddingBottom: 15,
     },
     text: {
-        color: theme.appBar.textPrimary,
+        color: theme.appBar.textSecondary,
+        paddingHorizontal: 10,
     },
+    active: {
+        color: theme.appBar.textPrimary,
+    }
 });
+
+const AppBarTab = ({ active, children, to }) => {
+    const textStyles = [styles.text, active && styles.active]
+    return (
+        <Link to={to} >
+            <StyledText fontWeight="bold" style={textStyles}>
+                {children}
+            </StyledText>
+        </Link>
+    );
+}
 
 const AppBar = () => {
     return (
         <View style={styles.container}>
-            <StyledText fontWeight="bold" style={styles.text}>
-                Repositories
-            </StyledText>
-        </View>
+            <ScrollView horizontal style={styles.scroll}>
+            <AppBarTab active to='/'>Repositories</AppBarTab>
+            <AppBarTab to='/signin'>Sign in</AppBarTab>
+            </ScrollView>
+       </View>
     );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Routes, Route, Navigate } from "react-router-native";
 
 const Main = () => {
     return (
         <View style={{ flex: 1 }}>
             <AppBar />
-            <RepositoryList />
+            <Routes>
+                <Route path="/" element= {<RepositoryList />} />
+                <Route path="/signin" element={<Text>Sign in</Text>}/>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
         </View>
     );
 };
+
+
 
 export default Main;
