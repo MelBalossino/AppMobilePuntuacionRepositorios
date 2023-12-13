a26580d9e96f22459a7b5dfcfe703ccb59299291
Melodi Balossino <melodibalossino@gmail.com>
2023-12-13 10:53:10 -0300

    StyledTextInput

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, TextInput } from 'react-native'
+import { View, TextInput, Button } from 'react-native'
 import { Formik } from 'formik'
 
 const initialValues = {
@@ -10,11 +10,22 @@ const initialValues = {
 const Login = () => {
     return (
         <Formik initialValues={initialValues} onSubmit={values => console.log(values)}>
-        {() => {
+        {( {onChangeText, handleChange, handleSubmit, values} ) => {
             return (
                 <View>
+                    <TextInput 
+                    placeholder='Email'
+                    values='{values.email}'
+                    onChangeText={handleChange('email')}
+                    />
                     <TextInput />
+                    <TextInput 
+                    placeholder='Password'
+                    values='{values.password}'
+                    onChangeText={handleChange('password')}
+                    />
                     <TextInput />
+                    <Button onPress={handleSubmit} title='Log In'/>
                 </View>
             )
         }}
---
+++ src/components/StyledTextInput.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginBottom: 10,
+    },
+    error: {
+        borderColor: "#d73a4a",
+    },
+});
+
+const StyledTextInput = ({ style = {}, error, ...props }) => {
+    const inputStyle = [
+        style,
+        styles.textInput,
+        error && styles.error
+    ];
+    return <TextInput style={inputStyle} {...props} />;
+}
+
+export default StyledTextInput;
